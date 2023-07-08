f54e3fa392046c68c55782d93d28bd451c4b446f
Haris Lania <haris.lania@gmail.com>
2023-07-09 01:41:51 +0500

    dependency resolver added

--- Global.asax.cs
+++ Global.asax.cs
@@ -16,6 +16,7 @@ namespace MNC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DependencyResolver.SetResolver(new MNCDependencyResolver());
         }
     }
 }
---
+++ MNCDependencyResolver.cs
@@ -0,0 +1,57 @@
+﻿using MNC.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Unity;
+
+namespace MNC
+{
+    public class MNCDependencyResolver : IDependencyResolver
+    {
+        private readonly IUnityContainer container;
+
+        public MNCDependencyResolver()
+        {
+            container = new UnityContainer();
+            RegisterDependencies();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return container.Resolve(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return container.ResolveAll(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return new List<object>();
+            }
+        }
+
+        private void RegisterDependencies()
+        {
+            // Register your dependencies using the container
+            container.RegisterType<DepartmentRepository>();
+            container.RegisterType<CustomerRepository>();
+            container.RegisterType<ItemRepository>();
+            container.RegisterType<EmployeeRepository>();
+            container.RegisterType<OrderRepository>();
+            // Register other dependencies
+        }
+    }
+}
